4ff2052f78193a8d0cb229da6251a492741326ea
GD0122 <galuhdental55@gmail.com>
2024-04-28 22:46:48 +0700

    cobaup 10

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ morgan.token('body', (req: Request, res: Response) => JSON.stringify(req.body));
 // Custom token untuk mendapatkan nilai dari req.query
 morgan.token('query', (req: Request, res: Response) => JSON.stringify(req.query));
 
-const accessLogStream = fs.createWriteStream('./LOGGER/access.log', { flags: 'a' });
+const accessLogStream = fs.createWriteStream('./Logger/access.log', { flags: 'a' });
 app.use(morgan((tokens:any, req: Request, res: Response) => {
     const currentDate = new Date().toISOString();
     return [
